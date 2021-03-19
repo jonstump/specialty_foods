2df5b2fa97a8c252eaffbf1710838ac4f1f1b04e
Jon Stump <jmstump@gmail.com>
2021-03-19 16:31:54 -0700

    add passing integration test for deleting reviews

---
+++ spec/features/delete_review_pages_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe 'the delete review process' do
+
+  it "deletes a product" do
+    @product = Product.new({ :name => "Apple", :price => "1.99", :country_of_origin => "United States" })
+    @product.save
+    visit products_path
+    click_on 'Apple'
+    click_link 'Add a review'
+    fill_in 'Author', :with => 'Oliver Queen'
+    fill_in 'Rating', :with => '5'
+    fill_in 'Content body', :with => 'These are perfect for practicing shooting my bow. They are also delicious!'
+    click_on 'Create Review'
+    click_link 'Oliver Queen'
+    click_on 'Delete review'
+    expect(page).to have_content 'Review successfully deleted'
+    expect(page).to have_no_content 'Oliver Queen'
+  end
+end
+
