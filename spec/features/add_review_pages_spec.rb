1f05c8b26a0458a1211928f887a274ec6a093a55
Jon Stump <jmstump@gmail.com>
2021-03-19 15:37:17 -0700

    dry up code and add tests for other errors

--- spec/features/add_review_pages_spec.rb
+++ spec/features/add_review_pages_spec.rb
@@ -1,10 +1,14 @@
 require 'rails_helper'
 
+
 describe "the add review process" do
+  before(:each) do
+      @product = Product.new({ :name => "Apple", :price => "1.99", :country_of_origin => "United States" })
+      @product.save
+      visit products_path
+  end
+
   it "adds a new review" do
-    @product = Product.new({ :name => "Apple", :price => "1.99", :country_of_origin => "United States" })
-    @product.save
-    visit products_path
     click_link 'Apple'
     click_link 'Add a review'
     fill_in 'Author', :with => 'Oliver Queen'
@@ -16,9 +20,6 @@ describe "the add review process" do
   end
 
   it "gives an error when no author is entered" do
-    @product = Product.new({ :name => "Apple", :price => "1.99", :country_of_origin => "United States" })
-    @product.save
-    visit products_path
     click_link 'Apple'
     click_on 'Add a review'
     click_on 'Create Review'
@@ -26,9 +27,6 @@ describe "the add review process" do
   end
 
   it "gives an error when no rating is entered" do
-    @product = Product.new({ :name => "Apple", :price => "1.99", :country_of_origin => "United States" })
-    @product.save
-    visit products_path
     click_link 'Apple'
     click_on 'Add a review'
     fill_in 'Author', :with => 'Oliver Queen'
@@ -37,15 +35,42 @@ describe "the add review process" do
     expect(page).to have_content "Rating can't be blank"
   end
 
+  it "gives an error when rating is not equal to or less than 5" do
+    click_link 'Apple'
+    click_on 'Add a review'
+    fill_in 'Author', :with => 'Oliver Queen'
+    fill_in 'Rating', :with => '8'
+    fill_in 'Content body', :with => 'These are perfect for practicing shooting my bow. They are also delicious!'
+    click_on 'Create Review'
+    expect(page).to have_content "Rating must be less than or equal to 5"
+  end
+
   it "gives an error when no content body is entered" do
-    @product = Product.new({ :name => "Apple", :price => "1.99", :country_of_origin => "United States" })
-    @product.save
-    visit products_path
     click_link 'Apple'
     click_on 'Add a review'
     fill_in 'Author', :with => 'Oliver Queen'
     fill_in 'Rating', :with => '5'
     click_on 'Create Review'
     expect(page).to have_content "Content body can't be blank"
+  end
+
+  it "gives an error when no content body is not 50 characters long" do
+    click_link 'Apple'
+    click_on 'Add a review'
+    fill_in 'Author', :with => 'Oliver Queen'
+    fill_in 'Rating', :with => '5'
+    fill_in 'Content body', :with => 'These are perfect for practicing shooting my bow.'
+    click_on 'Create Review'
+    expect(page).to have_content "Content body is too short (minimum is 50 characters)"
+  end
+
+  it "gives an error when no content body is over 250 characters long" do
+    click_link 'Apple'
+    click_on 'Add a review'
+    fill_in 'Author', :with => 'Oliver Queen'
+    fill_in 'Rating', :with => '5'
+    fill_in 'Content body', :with => 'These apples were amazing. They were juicy with a crisp taste. They are fantastic for shooting practice with my bow. I love that they are green which is my favorite color. I am less of a fan of red apples. Make sure to buy these by the dozen because that is the best deal!'
+    click_on 'Create Review'
+    expect(page).to have_content "Content body is too long (maximum is 250 characters)"
   end
 end
