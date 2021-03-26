7f73fb6254fcc5e4afd13b1afacebf584b604b64
Jon Stump <jmstump@gmail.com>
2021-03-26 09:22:42 -0700

    add user table created by devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_19_160747) do
+ActiveRecord::Schema.define(version: 2021_03_19_155853) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,14 +23,4 @@ ActiveRecord::Schema.define(version: 2021_03_19_160747) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "reviews", force: :cascade do |t|
-    t.string "author"
-    t.integer "rating"
-    t.string "content_body"
-    t.integer "product_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_foreign_key "reviews", "products"
 end
