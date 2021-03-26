061e0793b60b2d7e5bfaba5796cd53b35620d014
Jon Stump <jmstump@gmail.com>
2021-03-26 14:57:22 -0700

    add passing integration test for deleting a product

--- spec/features/add_product_pages_spec.rb
+++ spec/features/add_product_pages_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 describe 'the add product process as admin' do
-  before (:each) do
-    user = User.create!(:email => 'superman@email.com', :password => 'krypton', :admin => true)
-    login_as(user, :scope => :user)
+  before(:each) do
+    user = User.create!(email: 'superman@email.com', password: 'krypton', admin: true)
+    login_as(user, scope: :user)
   end
   it 'adds a new product' do
     visit products_path
     click_link 'Create new product'
-    fill_in 'Name', :with => 'Spaghetti'
-    fill_in 'Price', :with => '3.99'
-    fill_in 'Country of origin', :with => 'Italy'
+    fill_in 'Name', with: 'Spaghetti'
+    fill_in 'Price', with: '3.99'
+    fill_in 'Country of origin', with: 'Italy'
     click_on 'Create Product'
     expect(page).to have_content 'Product successfully created'
     expect(page).to have_content 'Spaghetti'
@@ -24,16 +24,16 @@ describe 'the add product process as admin' do
 
   it 'gives an error when no Country of origin is entered' do
     visit new_product_path
-    fill_in 'Name', :with => 'Spaghetti'
-    fill_in 'Price', :with => '3.99'
+    fill_in 'Name', with: 'Spaghetti'
+    fill_in 'Price', with: '3.99'
     click_on 'Create Product'
     expect(page).to have_content "Country of origin can't be blank"
   end
 
   it 'gives an error when no price is entered' do
     visit new_product_path
-    fill_in 'Name', :with => 'Spaghetti'
-    fill_in 'Country of origin', :with => 'Italy'
+    fill_in 'Name', with: 'Spaghetti'
+    fill_in 'Country of origin', with: 'Italy'
     click_on 'Create Product'
     expect(page).to have_content "Price can't be blank"
   end
@@ -41,11 +41,11 @@ end
 
 describe 'tests that a regular user cannot add a product' do
   before(:each) do
-    user = User.create!(:email => 'oliver@queenindustries.com', :password => 'dinah_lance')
-    login_as(user, :scope => :user)
+    user = User.create!(email: 'oliver@queenindustries.com', password: 'dinah_lance')
+    login_as(user, scope: :user)
   end
   it 'does not show add product for a standard user' do
     visit products_path
-    expect(page). to have_no_content 'create new product'
+    expect(page).to have_no_content 'create new product'
   end
 end
--- spec/features/delete_product_pages_spec.rb
+++ spec/features/delete_product_pages_spec.rb
@@ -1,13 +1,15 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# describe 'the delete product process' do
-#   it 'deletes a product' do
-#     @product = Product.new({ :name => 'Apple', :price => '1.99', :country_of_origin => 'United States' })
-#     @product.save
-#     visit products_path
-#     click_on 'Apple'
-#     click_on 'Delete'
-#     expect(page).to have_content 'Product successfully deleted'
-#     expect(page).to have_no_content 'Apple'
-#   end
-# end
+describe 'the delete product process' do
+  it 'deletes a product' do
+    user = User.create!(email: 'superman@email.com', password: 'krypton', admin: true)
+    login_as(user, scope: :user)
+    @product = Product.new({ name: 'Apple', price: '1.99', country_of_origin: 'United States' })
+    @product.save
+    visit products_path
+    click_on 'Apple'
+    click_on 'Delete'
+    expect(page).to have_content 'Product successfully deleted'
+    expect(page).to have_no_content 'Apple'
+  end
+end
