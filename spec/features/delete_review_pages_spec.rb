08cfcddb1ed3ce60293a2d4d0aae058aa662b60d
Jon Stump <jmstump@gmail.com>
2021-03-26 15:32:47 -0700

    add passing integration test for deleting a review as an admin

--- spec/features/delete_review_pages_spec.rb
+++ spec/features/delete_review_pages_spec.rb
@@ -1,20 +1,19 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# describe 'the delete review process' do
-
-#   it 'deletes a product' do
-#     @product = Product.new({ :name => "Apple", :price => "1.99", :country_of_origin => "United States" })
-#     @product.save
-#     visit products_path
-#     click_on 'Apple'
-#     click_link 'Add a review'
-#     fill_in 'Author', :with => 'Oliver Queen'
-#     fill_in 'Rating', :with => '5'
-#     fill_in 'Content body', :with => 'These are perfect for practicing shooting my bow. They are also delicious!'
-#     click_on 'Create Review'
-#     click_link 'Oliver Queen'
-#     click_on 'Delete review'
-#     expect(page).to have_content 'Review successfully deleted'
-#     expect(page).to have_no_content 'Oliver Queen'
-#   end
-# end
+describe 'the delete review process' do
+  it 'deletes a product' do
+    user = User.create!(email: 'superman@email.com', password: 'krypton', admin: true)
+    login_as(user, scope: :user)
+    @product = Product.new({ name: 'Apple', price: '1.99', country_of_origin: 'United States' })
+    @product.save
+    visit products_path
+    click_on 'Apple'
+    click_link 'Add a review'
+    fill_in 'Rating', with: '5'
+    fill_in 'Content body', with: 'These are perfect for practicing shooting my bow. They are also delicious!'
+    click_on 'Create Review'
+    click_link '1'
+    click_on 'Delete review'
+    expect(page).to have_content 'Review successfully deleted'
+  end
+end
