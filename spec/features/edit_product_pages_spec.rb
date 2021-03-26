1e86d55f408389aec6eb2635949a72dc91942421
Jon Stump <jmstump@gmail.com>
2021-03-26 15:55:42 -0700

    add passing integration tests for editing products as an admin

--- spec/features/edit_product_pages_spec.rb
+++ spec/features/edit_product_pages_spec.rb
@@ -1,53 +1,55 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# describe 'test the edit product process' do
-#   before(:each) do
-#     @product = Product.new({ :name => 'Apple', :price => '1.99', :country_of_origin => 'United States' })
-#     @product.save
-#     visit products_path
-#   end
+describe 'test the edit product process' do
+  before(:each) do
+    user = User.create!(email: 'superman@email.com', password: 'krypton', admin: true)
+    login_as(user, scope: :user)
+    @product = Product.new({ name: 'Apple', price: '1.99', country_of_origin: 'United States' })
+    @product.save
+    visit products_path
+  end
 
-#   it 'edits a product' do
-#     click_link 'Apple'
-#     click_link 'Edit'
-#     fill_in 'Name', :with => 'Orange'
-#     fill_in 'Price', :with => '3.99'
-#     fill_in 'Country of origin', :with => 'Italy'
-#     click_on 'Update Product'
-#     expect(page).to have_content 'Product successfully updated'
-#     expect(page).to have_content 'Orange'
-#     click_on 'Orange'
-#     expect(page).to have_content '$3.99'
-#     expect(page).to have_content 'Italy'
-#   end
+  it 'edits a product' do
+    click_link 'Apple'
+    click_link 'Edit'
+    fill_in 'Name', with: 'Orange'
+    fill_in 'Price', with: '3.99'
+    fill_in 'Country of origin', with: 'Italy'
+    click_on 'Update Product'
+    expect(page).to have_content 'Product successfully updated'
+    expect(page).to have_content 'Orange'
+    click_on 'Orange'
+    expect(page).to have_content '$3.99'
+    expect(page).to have_content 'Italy'
+  end
 
-#   it 'gives an error when no name is entered' do
-#     click_link 'Apple'
-#     click_link 'Edit'
-#     fill_in 'Name', :with => ' '
-#     fill_in 'Price', :with => '3.99'
-#     fill_in 'Country of origin', :with => 'Italy'
-#     click_on 'Update Product'
-#     expect(page).to have_content "Name can't be blank"
-#   end
+  it 'gives an error when no name is entered' do
+    click_link 'Apple'
+    click_link 'Edit'
+    fill_in 'Name', with: ' '
+    fill_in 'Price', with: '3.99'
+    fill_in 'Country of origin', with: 'Italy'
+    click_on 'Update Product'
+    expect(page).to have_content "Name can't be blank"
+  end
 
-#   it 'gives an error when no price is entered' do
-#     click_link 'Apple'
-#     click_link 'Edit'
-#     fill_in 'Name', :with => 'Orange'
-#     fill_in 'Price', :with => ' '
-#     fill_in 'Country of origin', :with => 'Italy'
-#     click_on 'Update Product'
-#     expect(page).to have_content "Price can't be blank"
-#   end
+  it 'gives an error when no price is entered' do
+    click_link 'Apple'
+    click_link 'Edit'
+    fill_in 'Name', with: 'Orange'
+    fill_in 'Price', with: ' '
+    fill_in 'Country of origin', with: 'Italy'
+    click_on 'Update Product'
+    expect(page).to have_content "Price can't be blank"
+  end
 
-#   it 'gives an error when no Country of origin is entered' do
-#     click_link 'Apple'
-#     click_link 'Edit'
-#     fill_in 'Name', :with => 'Orange'
-#     fill_in 'Price', :with => '3.99'
-#     fill_in 'Country of origin', :with => '  '
-#     click_on 'Update Product'
-#     expect(page).to have_content "Country of origin can't be blank"
-#   end
-# end
+  it 'gives an error when no Country of origin is entered' do
+    click_link 'Apple'
+    click_link 'Edit'
+    fill_in 'Name', with: 'Orange'
+    fill_in 'Price', with: '3.99'
+    fill_in 'Country of origin', with: '  '
+    click_on 'Update Product'
+    expect(page).to have_content "Country of origin can't be blank"
+  end
+end
